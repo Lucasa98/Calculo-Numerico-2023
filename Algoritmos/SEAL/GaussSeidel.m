af241b9dec65e155a7182fdeffc45ee9b5383b12
Lucas Saurin <lusaurin@gmail.com>
2023-05-04 00:34:40 -0300

    .

--- Algoritmos/SEAL/GaussSeidel.m
+++ Algoritmos/SEAL/GaussSeidel.m
@@ -1,28 +1,6 @@
-## Copyright (C) 2023 lusau
-##
-## This program is free software: you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see <https://www.gnu.org/licenses/>.
+function [x,it,r,t] = GaussSeidel (A,b,x0,tolerancia,maxit)
+  tic();
 
-## -*- texinfo -*-
-## @deftypefn {} {@var{retval} =} GaussSeidel (@var{input1}, @var{input2})
-##
-## @seealso{}
-## @end deftypefn
-
-## Author: lusau <lusau@LUCAS>
-## Created: 2023-04-13
-
-function [x,it,r] = GaussSeidel (A,b,x0,tolerancia,maxit)
   n=length(A(1,:));
 
   x=x0;
@@ -34,6 +12,8 @@ function [x,it,r] = GaussSeidel (A,b,x0,tolerancia,maxit)
                  %Gauss-Seidel  ^^^^^^^^
     endfor
 
+    %error relativo:
+    %  norm(x-x0)/norm(x);
     r(it+1) = norm(A*x-b);
     if r(it+1) < tolerancia
       break;
@@ -42,4 +22,6 @@ function [x,it,r] = GaussSeidel (A,b,x0,tolerancia,maxit)
     x0=x;
     it=it+1;
   endwhile
+
+  t = toc();
 endfunction
--- Algoritmos/SEAL/wOptimo.m
+++ Algoritmos/SEAL/wOptimo.m
@@ -1,27 +1,3 @@
-## Copyright (C) 2023 lusau
-##
-## This program is free software: you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-## -*- texinfo -*-
-## @deftypefn {} {@var{retval} =} wOptimo (@var{input1}, @var{input2})
-##
-## @seealso{}
-## @end deftypefn
-
-## Author: lusau <lusau@LUCAS>
-## Created: 2023-04-23
-
 function [w] = wOptimo (A,b,x0,tolerancia,maxit)
 
   aux=100000;
@@ -33,5 +9,9 @@ function [w] = wOptimo (A,b,x0,tolerancia,maxit)
       aux=it;
     endif
   endfor
+
+  %tambien se puede usar la formula, si la matriz es definida positiva
+  %y tridiagonal
+  %w = 2/( 1+sqrt( 1 - (RadJacobi(A)^2) ) )
 
 endfunction
